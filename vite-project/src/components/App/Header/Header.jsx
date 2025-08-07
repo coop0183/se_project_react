a36b4da1f5eaf15ab4e96b20de156518ab321d15
coop0183 <coop0183@gmail.com>
2025-08-07 18:44:49 -0500

    modified:   vite-project/src/components/App/App.jsx 	modified:   vite-project/src/components/App/Header/Header.jsx 	new file:   vite-project/src/components/App/ItemModal/ItemModal.css 	new file:   vite-project/src/components/App/ItemModal/ItemModal.jsx 	modified:   vite-project/src/components/App/ModalWithForm/ModalWithForm.css 	modified:   vite-project/src/components/App/ModalWithForm/ModalWithForm.jsx

--- vite-project/src/components/App/Header/Header.jsx
+++ vite-project/src/components/App/Header/Header.jsx
@@ -2,12 +2,12 @@ import './Header.css';
 import logo from '../../../assets/logo.svg';
 import avatar from '../../../assets/avatar.png';
 
-function Header() {
+function Header({ handleAddClick }) {
   return (
     <header className="header">
       <img className="header__logo" src={logo} alt="header logo" />
       <p className="header__date-and-location">DATE, LOCATION</p>
-      <button className="header__add-clothes-btn">+ Add clothes</button>
+      <button onClick={handleAddClick} type="button" className="header__add-clothes-btn">+ Add clothes</button>
       <div className="header__user-container">
         <p className="header__user-name">Terrence Tegegne</p>
         <img className="header__avatar" src={avatar} alt="Terrence Tegegne" />
